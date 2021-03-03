b3f07e4573f99671ad4c0c8c4057fa64c114ef96
Robert Lacher <robert.lacher@gmail.com>
2021-03-03 14:55:25 -0500

    updates

--- app-services/azure-ad-auth/deploy.ps1
+++ app-services/azure-ad-auth/deploy.ps1
@@ -8,12 +8,12 @@ $domainId="robertlachergmail.onmicrosoft.com"
 # Run Terraform and create infrastructure
 Expand-Archive terraform_0.14.7_linux_amd64.zip -DestinationPath /bin
 chmod u+x /bin/terraform
-terraform init
+terraform init -input=false
 $Env:TF_VAR_name=$Env:AppName
 $Env:TF_VAR_location=$Env:Location
 $Env:TF_VAR_tenant=$tenantId
 $Env:TF_VAR_domain=$domainId
-terraform apply -auto-approve -no-color
+terraform apply -auto-approve -no-color -input=false
 
 $clientId=$(terraform output -raw clientId)
 $redirectUri=$(terraform output -raw redirectUri)
@@ -28,7 +28,7 @@ cd angular11-sample-app
 apt update
 apt install -y python3
 python3 -c "import sys,json; j = json.load(open('auth-config.json', 'r')); j['credentials']['clientId'] = sys.argv[1]; j['credentials']['tenantId'] = sys.argv[2]; j['configuration']['redirectUri'] = sys.argv[3]; j['configuration']['postLogoutRedirectUri'] = sys.argv[3]; j['resources']['todoListApi']['resourceUri'] = sys.argv[4]; j['resources']['todoListApi']['resourceScopes'][0] = sys.argv[5]; json.dump(json.dumps(json.dumps(j)), open('tmp.json', 'w'))" $clientId $tenantId $redirectUri $resourceUri $resourceScope
-sed -i 's#module.exports = JSON.parse(.*#module.exports = JSON.parse('"$(cat tmp.json)"');#g' main.js
+sed -i 's/module.exports = JSON.parse(.*/module.exports = JSON.parse('"$(cat tmp.json)"');/g' main.js
 sed -i 's/""{/"{/g' main.js
 sed -i 's/}""/}"/g' main.js
 
