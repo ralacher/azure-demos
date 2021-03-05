1fc07d045bfc0eccd45830bd8b90a01891fd593f
Robert Lacher <robert.lacher@gmail.com>
2021-03-05 02:15:55 -0500

    updates

--- app-services/azure-ad-auth/main.tf
+++ app-services/azure-ad-auth/main.tf
@@ -12,12 +12,12 @@ terraform {
       source  = "hashicorp/random"
       version = "=3.1.0"
     }
-    backend "azurerm" {
-      resource_group_name  = "Identity-RG"
-      storage_account_name = "STORAGE_ACCOUNT_NAME"
-      container_name       = "tfstate"
-      key                  = "foundation.tfstate"
-    }
+  }
+  backend "azurerm" {
+    resource_group_name  = "Identity-RG"
+    storage_account_name = "STORAGE_ACCOUNT_NAME"
+    container_name       = "tfstate"
+    key                  = "foundation.tfstate"
   }
 }
 
